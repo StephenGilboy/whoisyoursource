7c4db7925e69fd10df01f077f73fa7dedbd1fa2f
Stephen Gilboy <stephen@gilboy.io>
2021-11-23 17:08:14 -0600

    Initial code to start Reddit auth

---
+++ src/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open RedditAuth
+
+[<EntryPoint>]
+let main argsv : int =
+    let creds = getRedditClientCredentials
+
+    match creds with
+    | Ok c -> displayCredentials c |> ignore
+    | Error msg -> printfn "Error: %s" msg
+    
+    0
