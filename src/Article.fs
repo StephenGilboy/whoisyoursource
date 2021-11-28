c174ef52975cd8dc08236d54d02fc30218704e40
Stephen Gilboy <stephen@gilboy.io>
2021-11-28 16:43:35 -0600

    Code to get links passes all local tests

--- src/Article.fs
+++ src/Article.fs
@@ -3,6 +3,109 @@ module Article
 open System
 open FSharp.Data
 
-let getLinks (htmlDocument: HtmlDocument) =
-    htmlDocument.Descendants ["a"]
+type Phrase = Phrase of string
 
+type Atag = {
+    Text: string
+    Link: string
+}
+
+type LinkTextIndex = {
+    Link: Atag
+    Index: int
+    Text: string
+}
+
+type PhraseTextIndex = {
+    Phrase: Phrase
+    Index: int
+    Text: string
+}
+
+type ParagraphPhrase = {
+    P: HtmlNode
+    Phrase: Phrase
+    Text: string
+    Index: int
+}
+
+type LinkPhraseDistance = {
+    Link: Atag
+    Phrase: Phrase
+    Distance: int
+}
+
+let findPhraseInText (phrase: string, text: string) =
+    let index = text.ToLower().IndexOf(phrase)
+    match index >= 0 with
+    | false -> None
+    | true -> Some {
+            Phrase = Phrase(phrase)
+            Text = text
+            Index = index
+        }
+
+let findPhrasesInText (phrases: List<string>, text: string) =
+    phrases
+    |> Seq.choose(fun p -> findPhraseInText (p, text))
+
+
+let findLinkInText (aTag: Atag, text: string) =
+    let aTagIndex = text.IndexOf(aTag.Text)
+    match aTagIndex >= 0 with 
+    | false -> None
+    | true -> Some {
+            Link = aTag
+            Text = text
+            Index = aTagIndex
+        }
+
+let findLinksInText (aTags: List<Atag>, text: string) =
+    aTags
+    |> Seq.choose(fun a -> findLinkInText(a, text))
+
+let getParagraphPhrases (htmlDocument: HtmlDocument, phrases: List<string>) =
+    htmlDocument.Descendants ["p"]
+    |> Seq.map (fun p -> 
+        let text = p.InnerText()
+        findPhrasesInText (phrases, text)
+        |> Seq.map (fun x -> 
+                {
+                    P = p 
+                    Phrase = x.Phrase
+                    Text = text
+                    Index = x.Index
+                }
+            )
+        )
+    |> Seq.reduce(fun a b -> Seq.append b a )
+
+
+let getLinksFromPtag (p: HtmlNode) =
+    p.Descendants ["a"]
+    |> Seq.map(fun a -> {
+        Text = a.InnerText()
+        Link = a.AttributeValue("href")
+        }
+     )
+
+let getSourceLinks (htmlDocument: HtmlDocument, phrases: List<string>) =
+    getParagraphPhrases (htmlDocument, phrases)
+    |> Seq.map(fun pp -> 
+            getLinksFromPtag pp.P
+            |> Seq.choose(fun l -> findLinkInText (l, pp.Text))
+            |> Seq.map(fun lt -> {
+                Link = lt.Link
+                Phrase = pp.Phrase
+                Distance = lt.Index - pp.Index |> Math.Abs
+            })
+            |> Seq.reduce(fun a b ->
+                match a.Distance <= b.Distance with
+                | true -> a
+                | false -> b)
+            )
+    |> Seq.reduce(fun a b ->
+        match a.Distance <= b.Distance with
+        | true -> a
+        | false -> b)
+    
--- tests/ArticleTest.fs
+++ tests/ArticleTest.fs
@@ -2,11 +2,59 @@ module ArticleTests
 
 open System
 open System.IO
+open Article
 open FSharp.Data
 open Xunit
 
-let getHtmlDocument filename =
-    let reader = new StreamReader filename
-    HtmlDocumen.load(reader)
+let getHtmlDocument (filename: string) =
+    let reader = new StreamReader(filename)
+    HtmlDocument.Load(reader)
 
-let 
+[<Fact>]
+let ``findPhraseInText should return Some PhraseTextIndex when it contains a given pharse`` () =
+    let phrase = "according to"
+    let text = "Something is making the news according to the AP."
+    let want = {
+        Phrase = Phrase(phrase)
+        Text = text
+        Index = 29
+    }
+    let pti = findPhraseInText (phrase, text)
+    match pti with
+    | Some got -> 
+        Assert.Equal(want, got)
+    | None -> Assert.True(false, "findPhraseInText returned None")
+
+[<Fact>]
+let ``findLinkInText should Some LinkTextIndex the link text is found in the text`` () =
+
+    let text = "Something is making the news says the AP."
+    let want = {
+        Link = {
+            Text = "the AP"
+            Link = "https://www.ap.org"
+        }
+        Text = text
+        Index = 34
+    }
+    let lti = findLinkInText (want.Link, text)
+    match lti with
+    | None -> Assert.True(false, "Link was not found in the text")
+    | Some got -> 
+        Assert.Equal(want, got)
+
+[<Fact>]
+let ``getSourceLink should return the link that the article is using for a source`` () =
+    let sourcePhrases = ["according to"; "reported"; "is reporting"; "published"]
+    let htmlDocument = getHtmlDocument "./test-data/accordingto.html"
+    let want = {
+        Link = {
+            Text = "Local News 11"
+            Link = "https://localnews11.com/original-story"
+        }
+        Phrase = Phrase("according to")
+        Distance = 13
+    }
+    let lpd = getSourceLinks (htmlDocument, sourcePhrases)
+    Assert.Equal(want, lpd)
+
